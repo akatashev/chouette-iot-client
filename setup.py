c1cad2a7a3aa6b75671d7b785350c958c634fd51
Artem Katashev <aharr@rowanleaf.net>
2020-06-09 02:43:15 +0100

    setup: Improve classifiers

--- setup.py
+++ setup.py
@@ -5,19 +5,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="chouette-client",
-    version="0.1.0",
+    version="0.0.1",
     author="Artem Katashev",
     author_email="aharr@rowanleaf.net",
     description="Python Client for Chouette-IoT metrics collection agent",
+    license="Apache License, Version 2.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/akatashev/chouette-iot-client",
     packages=setuptools.find_packages(),
     install_requires=["redis"],
-    classifierds=[
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "License:: OSI Approved:: Apache Software License",
-        "Operating System :: OS Independent"
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Monitoring"
     ],
     python_requires='>=3.6'
 )
